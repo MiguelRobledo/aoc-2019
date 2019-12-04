2d026e082641319b9d9545f64df37b8836846502
Miguel "Peppermint" Robledo <migroble@ucm.es>
2019-12-04 11:05:06 +0100

    day 4

---
+++ src/day4.rs
@@ -0,0 +1,73 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day4, part1)]
+pub fn input_gen_part1(input: &str) -> Vec<u64> {
+    input
+        .split("-")
+        .map(|c| u64::from_str_radix(c, 10).unwrap())
+        .collect()
+}
+
+#[aoc(day4, part1)]
+pub fn solve_part1(input: &[u64]) -> usize {
+    (input[0]..input[1])
+        .map(|n| n
+                .to_string()
+                .chars()
+                .zip(n
+                    .to_string()
+                    .chars()
+                    .skip(1)
+                )
+                .collect()
+        )
+        .filter(|v: &Vec<(char, char)>|
+            v
+                .iter()
+                .all(|(curr, next)| curr <= next)
+            &&
+            v
+                .iter()
+                .any(|(curr, next)| curr == next)
+        )
+        .count()
+}
+
+#[aoc_generator(day4, part2)]
+pub fn input_gen_part2(input: &str) -> Vec<u64> {
+    input_gen_part1(input)
+}
+
+
+#[aoc(day4, part2)]
+pub fn solve_part2(input: &[u64]) -> usize {
+    (input[0]..input[1])
+        .map(|n| n
+            .to_string()
+            .chars()
+            .zip(n
+                .to_string()
+                .chars()
+                .skip(1)
+            )
+            .collect()
+        )
+        .filter(|v: &Vec<(char, char)>|
+            v
+                .iter()
+                .all(|(curr, next)| curr <= next)
+            &&
+            v
+                .iter()
+                .fold((false, 1), |s, (curr, next)|
+                    match s {
+                        (true, _) => s,
+                        (false, 2) if curr != next => (true, 2),
+                        (false, c) if curr == next => (false, c + 1),
+                        (false, _) => (false, 1) 
+                    }
+                )
+                .1 == 2
+        )
+        .count()
+}
