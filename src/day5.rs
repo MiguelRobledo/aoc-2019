e12eca86eabc3fb1350ef736ee1fc2a2900004a1
Miguel "Peppermint" Robledo <migroble@ucm.es>
2019-12-05 08:52:29 +0100

    day 5 - refactor

--- src/day5.rs
+++ src/day5.rs
@@ -1,10 +1,76 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-fn get_value(mem: &[i64], pc: usize, n: u32) -> i64 {
-    if mem[pc] / 10_i64.pow(n + 1) % 10 == 1 {
-        mem[pc + n as usize]
-    } else {
-        mem[mem[pc + n as usize] as usize]
+pub struct Intcode {
+    mem: Vec<i64>,
+    pc: usize,
+    input: Vec<i64>
+}
+
+impl Intcode {
+    fn with_input(mem: Vec<i64>, input: Vec<i64>) -> Self {
+        Intcode { mem, pc: 0, input }
+    }
+    
+    fn get_arg(&self, n: u32) -> i64 {
+        if self.mem[self.pc] / 10_i64.pow(n + 1) % 10 == 1 {
+            self.mem[self.pc + n as usize]
+        } else {
+            self.mem[self.mem[self.pc + n as usize] as usize]
+        }
+    }
+    
+    pub fn run(&mut self) {
+        let mut it = self.input.iter();
+        
+        loop {
+            let opcode = self.mem[self.pc] % 100;
+            
+            match opcode {
+                1 | 2 | 7 | 8 => {
+                    let (x, y, z) = (
+                        self.get_arg(1),
+                        self.get_arg(2),
+                        self.mem[self.pc + 3] as usize
+                    );
+                    
+                    self.mem[z] = match opcode {
+                        1 => x + y,
+                        2 => x * y,
+                        7 => if x < y { 1 } else { 0 },
+                        8 => if x == y { 1 } else { 0 },
+                        _ => unreachable!()
+                    };
+                    
+                    self.pc += 4;
+                },
+                3 | 4 => {
+                    let x = self.mem[self.pc + 1] as usize;
+                    
+                    match opcode {
+                        3 => self.mem[x] = *it.next().unwrap(),
+                        4 => println!("{}", self.mem[x]),
+                        _ => unreachable!()
+                    }
+                    
+                    self.pc += 2;
+                },
+                5 | 6 => {
+                    let (x, y) = (
+                        self.get_arg(1),
+                        self.get_arg(2) as usize
+                    );
+                    
+                    if (opcode == 5 && x != 0) || (opcode == 6 && x == 0) {
+                        self.pc = y;
+                        continue;
+                    }
+                    
+                    self.pc += 3;
+                },
+                99 => break,
+                _ => panic!("invalid opcode {}", opcode),
+            }
+        }
     }
 }
 
@@ -19,96 +85,14 @@ pub fn input_gen(input: &str) -> Vec<i64> {
 
 #[aoc(day5, part1)]
 pub fn solve_part1(input: &[i64]) -> bool {
-    let data = vec![1];
-    let mut it = data.iter();
-    let mut mem = input.to_vec();
-    let mut pc = 0;
-    
-    loop {
-        let opcode = mem[pc] % 100;
-        
-        match opcode {
-            1 | 2 => {
-                let (x, y, z) = (get_value(&mem, pc, 1), get_value(&mem, pc, 2), mem[pc + 3] as usize);
-                
-                mem[z] = match opcode {
-                    1 => x + y,
-                    2 => x * y,
-                    _ => unreachable!()
-                };
-                
-                pc += 4;
-            },
-            3 | 4 => {
-                let x = mem[pc + 1] as usize;
-                
-                match opcode {
-                    3 => mem[x] = *it.next().unwrap(),
-                    4 => println!("{}", mem[x]),
-                    _ => unreachable!()
-                }
-                
-                pc += 2;
-            },
-            99 => break,
-            _ => panic!("invalid opcode {}", opcode),
-        };
-        
-    }
+    Intcode::with_input(input.to_vec(), vec![1]).run();
     
     true
 }
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &[i64]) -> bool {
-    let data = vec![5];
-    let mut it = data.iter();
-    let mut mem = input.to_vec();
-    let mut pc = 0;
-    
-    loop {
-        let opcode = mem[pc] % 100;
-        
-        match opcode {
-            1 | 2 | 7 | 8 => {
-                let (x, y, z) = (get_value(&mem, pc, 1), get_value(&mem, pc, 2), mem[pc + 3] as usize);
-                
-                mem[z] = match opcode {
-                    1 => x + y,
-                    2 => x * y,
-                    7 => if x < y { 1 } else { 0 },
-                    8 => if x == y { 1 } else { 0 },
-                    _ => unreachable!()
-                };
-                
-                pc += 4;
-            },
-            3 | 4 => {
-                let x = mem[pc + 1] as usize;
-                
-                match opcode {
-                    3 => mem[x] = *it.next().unwrap(),
-                    4 => println!("{}", mem[x]),
-                    _ => unreachable!()
-                }
-                
-                pc += 2;
-            },
-            5 | 6 => {
-                let (x, y) = (get_value(&mem, pc, 1), get_value(&mem, pc, 2) as usize);
-                
-                if (opcode == 5 && x != 0) || (opcode == 6 && x == 0) {
-                    pc = y;
-                    continue;
-                }
-                
-                pc += 3;
-            },
-            99 => break,
-            _ => panic!("invalid opcode {}", opcode),
-        };
-        
-    }
+    Intcode::with_input(input.to_vec(), vec![5]).run();
     
     true
 }
