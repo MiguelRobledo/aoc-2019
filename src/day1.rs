15edbf18a67d19d17a7d88428aea3ddf29997986
Miguel "Peppermint" Robledo <migroble@ucm.es>
2019-12-07 12:02:57 +0100

    Revert "day 1 - small refactor"
    This reverts commit b6ac3070d46e7c20612a6afad683babd72ce3c0e.

--- src/day1.rs
+++ src/day1.rs
@@ -12,7 +12,9 @@ pub fn input_gen(input: &str) -> Vec<i64> {
 pub fn solve_part1(input: &[i64]) -> i64 {
     input
         .iter()
-        .fold(0, |sum, m| sum + m / 3 - 2)
+        .fold(0, |sum, m| {
+            sum + m / 3 - 2
+        })
 }
 
 #[aoc(day1, part2)]
@@ -20,13 +22,13 @@ pub fn solve_part2(input: &[i64]) -> i64 {
     input
         .iter()
         .fold(0, |sum, m| {
-            let mut fuel = *m;
+            let mut fuel = m / 3 - 2;
             let mut total = 0;
             
             sum + loop {
                 if fuel > 0 {
-                    fuel = fuel / 3 - 2;
                     total += fuel;
+                    fuel = fuel / 3 - 2;
                 } else {
                     break total;
                 }
