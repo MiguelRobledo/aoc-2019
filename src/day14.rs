698201af78e2aadf598b8207f9208438076dacd6
Miguel "Peppermint" Robledo <migroble@ucm.es>
2019-12-14 14:22:46 +0100

    day 14 - clean up

--- src/day14.rs
+++ src/day14.rs
@@ -2,9 +2,7 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 use std::collections::HashMap;
 
-#[derive(Clone, Debug)]
 pub struct Recipe {
-    name: String,
     amount: u64,
     required: Vec<(String, u64)>
 }
@@ -12,16 +10,22 @@ pub struct Recipe {
 fn convert_to_ore(name: &str, amount: u64, recipes: &HashMap<String, Recipe>, leftovers: &mut HashMap<String, u64>) -> u64 {
     if let Some(recipe) = recipes.get(name) {
         let in_storage = *leftovers.entry(name.to_string()).or_insert(0);
-        let (leftover, needed) = if in_storage < amount { (0, amount - in_storage) } else { (in_storage - amount, 0) };
-        let repetitions = (needed + (recipe.amount - 1)) / recipe.amount;
-        let produced = recipe.amount * repetitions;
         
-        let extra = produced - needed;
-        let ingredients = &recipe.required;
+        let (leftover, needed) = if in_storage < amount {
+            (0, amount - in_storage)
+        } else {
+            (in_storage - amount, 0)
+        };
+        
+        let iterations = (needed + recipe.amount - 1) / recipe.amount;
+        let extra      = recipe.amount * iterations - needed;
         
         leftovers.insert(name.to_string(), leftover + extra);
         
-        ingredients.iter().map(|i| convert_to_ore(&i.0, i.1 * repetitions, recipes, leftovers)).sum()
+        recipe.required
+            .iter()
+            .map(|i| convert_to_ore(&i.0, i.1 * iterations, recipes, leftovers))
+            .sum()
     } else {
         amount
     }
@@ -35,12 +39,13 @@ fn calc_fuel(mut ore: u64, recipes: &HashMap<String, Recipe>, leftovers: &mut Ha
         let required = convert_to_ore("FUEL", amount, recipes, leftovers);
         
         if required > ore {
-            amount /= 10;
-            if amount == 0 {
+            if amount == 1 {
                 break fuel;
+            } else {
+                amount /= 10;
             }
         } else {
-            ore -= required;
+            ore  -= required;
             fuel += amount;
         }
     }
@@ -48,20 +53,21 @@ fn calc_fuel(mut ore: u64, recipes: &HashMap<String, Recipe>, leftovers: &mut Ha
 
 #[aoc_generator(day14)]
 pub fn input_gen(input: &str) -> HashMap<String, Recipe> {
+    fn parse_product(s: &str) -> (String, u64) {
+        let mut it = s.trim().split(' ');
+        let amount = u64::from_str_radix(it.next().unwrap(), 10).unwrap();
+        let name   = it.next().unwrap().to_string();
+        
+        (name, amount)
+    }
+    
     input
         .trim()
         .lines()
         .map(|l| {
-            let mut it = l.split("=>");
+            let mut it   = l.split("=>");
             let required = it.next().unwrap();
-            let product = it.next().unwrap();
-            let parse_product = |s: &str| {
-                let mut it = s.trim().split(' ');
-                let amount = u64::from_str_radix(it.next().unwrap(), 10).unwrap();
-                let name = it.next().unwrap().to_string();
-                
-                (name, amount)
-            };
+            let product  = it.next().unwrap();
             
             let (name, amount) = parse_product(product);
             let required = required
@@ -69,7 +75,7 @@ pub fn input_gen(input: &str) -> HashMap<String, Recipe> {
                 .map(|s| parse_product(s))
                 .collect();
             
-            (name.clone(), Recipe { name, amount, required })
+            (name, Recipe { amount, required })
         })
         .collect()
 }
